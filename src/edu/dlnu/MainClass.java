c559886edee30c929e6b2e98bf7d692707e947bf
Mr.Rac <111@222.com>
2018-11-30 10:11:20 +0800

    创建Java web工程

---
+++ src/edu/dlnu/MainClass.java
@@ -0,0 +1,19 @@
+package edu.dlnu;
+
+import org.junit.Test;
+
+public class MainClass {
+    //在类的{ }下
+    //属性
+    //方法
+    //主方法
+    public static void main(String[] args) {
+        System.out.println("咕咕咕");
+    }
+    //测试方法
+    @Test
+    public void test1(){
+        //创建一段HTML字符串
+        String html = "<html> <head> </head> <body>测试</body> </html>";
+    }
+}
